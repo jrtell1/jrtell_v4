04d6a98901fa5bc5b167ca2c23e2446ace79abb0
JRTell <john-robin@jrtell.se>
2019-03-10 18:22:31 +0100

    startup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,5 @@
 const pkg = require('./package');
 
-
 module.exports = {
   mode: 'universal',
 
@@ -8,14 +7,18 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'John-Robin Tell',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Cinzel+Decorative' },
+      // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Cormorant+Unicase' },
+      // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lekton' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=IM+Fell+English+SC' },
     ]
   },
 
@@ -27,9 +30,7 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [
-    '~/assets/css/tailwind.css'
-  ],
+  css: [],
 
   /*
   ** Plugins to load before mounting the App
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,96 @@
 <template>
+  <div class="hero">
   <section class="container">
     <div>
-      <logo />
+      <img class="portrait" src="https://res.cloudinary.com/jrtell/image/upload/w_200,h_200,c_fill,g_face/v1532716014/jrtell/Snapchat-483615629.jpg"/>
       <h1 class="title">
-        jrtell.se
+        John-Robin Tell
       </h1>
       <h2 class="subtitle">
-        jrtell.se
+        Fullstack Developer
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+        <a class="button--green" href="https://www.linkedin.com/in/john-robin-tell-b8bb235b/" target="_blank">LinkedIn</a>
+        <a class="button--grey" href="https://github.com/jrtell1" target="_blank">GitHub</a>
       </div>
     </div>
   </section>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  export default {
 
-export default {
-  components: {
-    Logo
   }
-}
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .portrait {
+    border-radius: 50%;
+    border: 5px solid #adadad;
+    overflow: hidden;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .title {
+    /*font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;*/
+    /*font-family: 'Cinzel Decorative', cursive;*/
+    /*font-family: 'Cormorant Unicase', serif;*/
+    font-family: 'IM Fell English SC', serif;
+    display: block;
+    font-weight: 300;
+    font-size: 60px;
+    letter-spacing: 1px;
+    color: white;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 32px;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+    color: white;
+  }
+
+  .links {
+    padding-top: 15px;
+  }
+
+  .hero {
+    min-height: 100vh;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    text-align: center;
+    /*background: linear-gradient(rgba(23,23,56, 0.8), rgba(23,23,56, 0.8)), url("https://res.cloudinary.com/jrtell/image/upload/c_crop,fl_progressive,q_auto,w_1920,h_1080/v1552236316/jrtell/bg2.jpg") top no-repeat;*/
+    /*background: linear-gradient(rgba(25,19,8, 0.8), rgba(25,19,8, 0.8)), url("https://res.cloudinary.com/jrtell/image/upload/c_crop,fl_progressive,q_auto,w_1920,h_1080/v1552236316/jrtell/bg2.jpg") top no-repeat;*/
+    /*background: linear-gradient(rgba(14,16,61, 0.75), rgba(14,16,61, 0.75)), url("https://res.cloudinary.com/jrtell/image/upload/c_crop,fl_progressive,q_auto,w_1920,h_1080/v1552236316/jrtell/bg2.jpg") top no-repeat;*/
+    background: linear-gradient(rgba(2,2,2, 0.75), rgba(2,2,2, 0.75)), url("https://res.cloudinary.com/jrtell/image/upload/c_crop,fl_progressive,q_auto,w_1920,h_1080/v1552236316/jrtell/bg2.jpg") top no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+  }
+
+  @media screen and (max-width: 600px) {
+    .title {
+      font-size: 40px;
+    }
+
+    .subtitle {
+      font-size: 22px;
+    }
+  }
 </style>
