6c29b308dbce4b20ad16cd7e93d6a4051a0d2205
John <john-robin@jrtell.se>
2019-05-29 19:34:04 +0200

    Extracted proficiencies to own child component

---
+++ pages/-index-proficiencies.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <h3 class="section__title">Proficiencies</h3>
+    <div class="proficiencies">
+      <div class="proficiencies__column">
+        <h4 class="proficiencies__title">Languages</h4>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>PHP</li>
+          <li>SQL</li>
+        </ul>
+      </div>
+      <div class="proficiencies__column">
+        <h4 class="proficiencies__title">Frameworks</h4>
+        <ul>
+          <li>VueJs</li>
+          <li>NuxtJs</li>
+          <li>Laravel</li>
+          <li>Socket.io</li>
+          <li>Tailwindcss</li>
+          <li>Bulma</li>
+        </ul>
+      </div>
+      <div class="proficiencies__column">
+        <h4 class="proficiencies__title">Tools</h4>
+        <ul>
+          <li>Docker</li>
+          <li>GitHub</li>
+          <li>Netlify</li>
+          <li>Cloudinary CDN</li>
+          <li>Webpack</li>
+          <li>Nginx</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'proficiencies'
+  };
+</script>
+
+<style scoped>
+  .proficiencies {
+    display: flex;
+  }
+
+  .proficiencies__column {
+    flex: 1;
+  }
+
+
+  .proficiencies__title {
+    font-size: 18px;
+    font-weight: 500;
+  }
+
+  .proficiencies__column ul {
+    padding: 0;
+  }
+
+  .proficiencies__column li {
+    list-style: none;
+    padding: 2px 10px;
+  }
+
+  .proficiencies__column li::before {
+    border-radius: 3px;
+    content: "";
+    display: inline-block;
+    height: 10px;
+    width: 10px;
+    margin-right: 5px;
+    background: #1c5ca0;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -46,41 +46,7 @@
 
       <section class="section">
         <div class="container">
-          <h3 class="section__title">Proficiencies</h3>
-          <div class="proficiencies">
-            <div class="proficiencies__column">
-              <h4 class="proficiencies__title">Languages</h4>
-              <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>PHP</li>
-                <li>SQL</li>
-              </ul>
-            </div>
-            <div class="proficiencies__column">
-              <h4 class="proficiencies__title">Frameworks</h4>
-              <ul>
-                <li>VueJs</li>
-                <li>NuxtJs</li>
-                <li>Laravel</li>
-                <li>Socket.io</li>
-                <li>Tailwindcss</li>
-                <li>Bulma</li>
-              </ul>
-            </div>
-            <div class="proficiencies__column">
-              <h4 class="proficiencies__title">Tools</h4>
-              <ul>
-                <li>Docker</li>
-                <li>GitHub</li>
-                <li>Netlify</li>
-                <li>Cloudinary CDN</li>
-                <li>Webpack</li>
-                <li>Nginx</li>
-              </ul>
-            </div>
-          </div>
+          <proficiencies/>
         </div>
       </section>
     </div>
@@ -89,9 +55,10 @@
 
 <script>
   import IndexProjects from './-index-projects';
+  import Proficiencies from './-index-proficiencies';
 
   export default {
-    components: { IndexProjects }
+    components: { Proficiencies, IndexProjects }
   }
 </script>
 
@@ -228,39 +195,6 @@
     justify-content: center;
     align-items: center;
     text-align: center;
-  }
-
-  .proficiencies {
-    display: flex;
-  }
-
-  .proficiencies__column {
-    flex: 1;
-  }
-
-
-  .proficiencies__title {
-    font-size: 18px;
-    font-weight: 500;
-  }
-
-  .proficiencies__column ul {
-    padding: 0;
-  }
-
-  .proficiencies__column li {
-    list-style: none;
-    padding: 2px 10px;
-  }
-
-  .proficiencies__column li::before {
-    border-radius: 3px;
-    content: "";
-    display: inline-block;
-    height: 10px;
-    width: 10px;
-    margin-right: 5px;
-    background: #1c5ca0;
   }
 
   .portrait--animation {
